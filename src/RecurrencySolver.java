51d067f3255bd972312fb12c43900ffa158be748
Piotr Gaiński <dociebieaniuszlem@gmail.com>
2018-12-29 10:32:49 +0100

    Add primitive spliterator

--- src/RecurrencySolver.java
+++ src/RecurrencySolver.java
@@ -1,10 +1,26 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-public class RecurrencySolver {
+public class RecurrencySolver{
     private List<Double> arguments;
     private SquareMatrix matrix;
     private int n;
+
+    int getN() {
+        return n;
+    }
+
+    SquareMatrix getMatrix(){
+        return new SquareMatrix(matrix);
+    }
+
+    List<Double> getArguments(){
+        return arguments;
+    }
 
     public void setRecurrency(Double [] recurrency){
         List<Double> rec = Arrays.asList(recurrency);
@@ -28,10 +44,72 @@ public class RecurrencySolver {
     public Double getKthValue(int k){
         if(k < n)
             return arguments.get(k);
-        SquareMatrix resultMatrix = getKthMatrix(k);
+        return extractValue(arguments, getKthMatrix(k));
+
+    }
+
+    static Double extractValue(List<Double> arguments, SquareMatrix matrix){
         Double result = 0.0;
-        for (int i = 0; i < resultMatrix.getN(); i++)
-            result += arguments.get(i) * resultMatrix.get(i, 0);
+        for (int i = 0; i < matrix.getN(); i++)
+            result += arguments.get(i) * matrix.get(i, 0);
         return result;
+    }
+
+    public RecurrencySolverSpliterator spliterator(){
+        return new RecurrencySolverSpliterator(this);
+    }
+
+    public Stream<Double> stream(){
+        return StreamSupport.stream(spliterator(), false);
+    }
+}
+
+class RecurrencySolverSpliterator implements Spliterator<Double>{
+
+    private SquareMatrix recMatrix;
+    private SquareMatrix currentMatrix;
+    private List<Double> arguments;
+    private int currentMatrixPower;
+    private int matrixDim;
+    private int current;
+    private int last;
+
+    RecurrencySolverSpliterator(RecurrencySolver solver){
+        this.current = 0;
+        this.currentMatrixPower = 1;
+        this.matrixDim = solver.getN();
+        this.arguments = solver.getArguments();
+        this.currentMatrix = solver.getMatrix();
+        this.recMatrix = solver.getMatrix();
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super Double> consumer) {
+        if(current < matrixDim)
+            consumer.accept(arguments.get(current));
+        else {
+            int wantedPower = current - matrixDim + 1;
+            int remainPower = wantedPower - currentMatrixPower;
+            currentMatrix = SquareMatrix.multiply(currentMatrix, SquareMatrix.power(recMatrix, remainPower));
+            currentMatrixPower = wantedPower;
+            consumer.accept(RecurrencySolver.extractValue(arguments, currentMatrix));
+        }
+        current++;
+        return true;
+    }
+
+    @Override
+    public Spliterator<Double> trySplit() {
+        return null;
+    }
+
+    @Override
+    public long estimateSize() {
+        return 0;
+    }
+
+    @Override
+    public int characteristics() {
+        return 0;
     }
 }
--- src/RecurrencySolverTest.java
+++ src/RecurrencySolverTest.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import static org.junit.Assert.*;
 
 public class RecurrencySolverTest {
@@ -12,5 +18,16 @@ public class RecurrencySolverTest {
         assertEquals(Double.valueOf(3.0), solver.getKthValue(3));
         assertEquals(Double.valueOf(610.0), solver.getKthValue(14));
         assertEquals(Double.valueOf(4181.0), solver.getKthValue(18));
+    }
+
+    @org.junit.Test
+    public void SpliteratorTest() {
+        RecurrencySolver solver = new RecurrencySolver();
+        solver.setRecurrency(new Double[]{1.0, 1.0});
+        solver.setArguments(new Double[]{1.0, 1.0});
+        List<Double> result = new ArrayList<>();
+        List<Double> expected = Arrays.asList(1.0, 1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0, 34.0, 55.0);
+        solver.stream().limit(10).forEach(result::add);
+        assertEquals(expected, result);
     }
 }
